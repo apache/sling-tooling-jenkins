b019d9414e84d64b2aa45f430a064148b270a6df
Robert Munteanu <rombert@apache.org>
2018-12-18 00:06:06 +0100

    SLING-7245 - Validate pull requests using Jenkins
    Extract parsing to a separate class

---
+++ src/sling/SlingModuleParser.groovy
@@ -0,0 +1,31 @@
+package sling;
+
+class SlingModuleParser {
+
+    def fileName;
+    
+    // defaults for the build
+    def buildDesc = {
+        jdks: [8],
+        downstreamProjects: [],
+        archivePatterns: [],
+        mavenGoal: 'install',
+        additionalMavenParams: '',
+        rebuildFrequency: '@weekly',
+        enableXvfb: false,
+        enabled: true
+    }
+
+    SlingModuleParser(fileName) {
+        this.fileName = fileName;        
+    }
+
+    def parse() {
+        def slingMod = new XmlParser().parse(overrides)
+        if ( slingMod?.jenkins?.jdks ) {
+            def jdks = []
+            slingMod.jenkins.jdks.jdk.each { jdks.add it.text() }
+            buildDesc.jdks = jdks
+        }
+    }
+}
