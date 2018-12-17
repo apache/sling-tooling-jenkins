45a8fb1587365cf083ac4a28150830ebb35b149d
Robert Munteanu <rombert@apache.org>
2018-12-18 00:06:06 +0100

    SLING-7245 - Validate pull requests using Jenkins
    Switch to scripted pipeline for dynamic stage definition and
    also use the 'withMaven' block for simpler configuration.

--- vars/slingOsgiBundleBuild.groovy
+++ vars/slingOsgiBundleBuild.groovy
@@ -1,36 +1,20 @@
 def call(Map params = [:]) {
     def moduleDir = params.containsKey('moduleDir') ? params.moduleDir : '.'
-    
-    def overrides = readFile('.sling-module.xml')
-    echo "Got overrides ${overrides}"
-    def missingFile = readFile("blah")
-    echo "Got missing file ${missingFile}"
 
-    pipeline {
-        agent {
-            label 'ubuntu'
+    node('ubuntu') {
+
+        checkout scm
+
+        stage('Init') {
+            def overrides = readFile('.sling-module.xml')
+            echo "Got overrides ${overrides}"
         }
 
-        tools {
-            maven 'Maven 3.3.9'
-            jdk 'JDK 1.8 (latest)'
-        }
-
-        stages {
-            stage ('Build') {
-                steps {
-                    dir(moduleDir) {
-                        sh 'mvn clean install' 
-                    }
-                }
-            }
-        }
-
-        post {
-            always {
+        stage('Build') {
+            withMaven(maven: 'Maven 3.3.9', jdk: 'JDK 1.8 (latest)' ) {
                 dir(moduleDir) {
-                    junit allowEmptyResults: true, testResults: '**/target/surefire-reports/*.xml,**/target/failsafe-reports/*.xml'
-                }
+                    sh 'mvn clean install' 
+                }                
             }
         }
     }
