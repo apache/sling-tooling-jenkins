ab91e3c899454ef65d88db5de7595d5b1708e0a1
Robert Munteanu <rombert@apache.org>
2018-12-18 00:06:06 +0100

    SLING-7245 - Validate pull requests using Jenkins
    Start parsing .sling-module.xml

--- vars/slingOsgiBundleBuild.groovy
+++ vars/slingOsgiBundleBuild.groovy
@@ -1,20 +1,39 @@
 def call(Map params = [:]) {
     def availableJDKs = [ 8: 'JDK 1.8 (latest)', 9: 'JDK 1.9 (latest)']
-    
-    def moduleDir = params.containsKey('moduleDir') ? params.moduleDir : '.'
     def mvnVersion = 'Maven 3.3.9'
-    def jdkVersions = [8, 9]
+    // defaults for the build
+    def buildDesc = {
+        jdks: [8],
+        downstreamProjects: [],
+        archivePatterns: [],
+        mavenGoal: 'install',
+        additionalMavenParams: '',
+        rebuildFrequency: '@weekly',
+        enableXvfb: false,
+        enabled: true
+    }
+
+    def moduleDir = params.containsKey('moduleDir') ? params.moduleDir : '.'
 
     node('ubuntu') {
 
         checkout scm
 
         stage('Init') {
-            def overrides = readFile('.sling-module.xml')
-            echo "Got overrides ${overrides}"
+            if ( fileExists('.sling-module.xml') ) {
+                def overrides = readFile('.sling-module.xml')
+                def slingMod = new XmlParser().parse(overrides)
+                if ( slingMod?.jenkins?.jdks ) {
+                    def jdks = []
+                    slingMod.jenkins.jdks.jdk.each { jdks.add it.text() }
+                    buildDesc.jdks = jdks
+
+                    echo "$Overriding JDKs list to be ${jdks}"
+                }
+            }
         }
 
-        jdkVersions.each { jdkVersion -> 
+        buildDesc.jdks.each { jdkVersion -> 
             stage("Build (Java ${jdkVersion})") {
                 def jenkinsJdkLabel = availableJDKs[jdkVersion]
                 if ( !jenkinsJdkLabel )
