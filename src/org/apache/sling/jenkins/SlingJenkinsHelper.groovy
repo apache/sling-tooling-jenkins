0001c046d986acc651f18f8c3700f456e973fe5f
Robert Munteanu <rombert@apache.org>
2019-04-15 12:44:58 +0300

    SLING-8262 -  Add SonarQube analysis to Jenkins jobs
    Attempt to read the repository URL to prepare the sonar.projectKey
    property.

--- src/org/apache/sling/jenkins/SlingJenkinsHelper.groovy
+++ src/org/apache/sling/jenkins/SlingJenkinsHelper.groovy
@@ -61,6 +61,8 @@ def runWithErrorHandling(Closure build) {
 
             stage('Init') {
                 checkout scm
+                def url = sh(returnStdout: true, script: 'git config remote.origin.url').trim()
+                echo "Detected SCM url ${url}"
                 if ( fileExists('.sling-module.json') ) {
                     overrides = readJSON file: '.sling-module.json'
                     echo "Jenkins overrides: ${overrides.jenkins}"
