b012331220307b68d6720617c98cc8a5d0fcbed6
Bertrand Delacretaz <bdelacretaz@apache.org>
2020-08-19 12:37:01 +0200

    Make script safe against running twice

--- move-to-ci-builds/change-links.sh
+++ move-to-ci-builds/change-links.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-sed 's/builds.apache/ci-builds.apache/g' \
+function fatal() {
+    echo $* >&2
+    exit 1
+}
+
+( awk '{if ($0 ~ /ci-builds/) { exit 1 } ; print $0 }' || fatal "Input was already processed?" ) \
+| sed 's/builds.apache/ci-builds.apache/g' \
 | sed 's/Sling\/job/Sling\/job\/modules\/job/g' \
 | sed 's/test_results_analyzer/lastCompletedBuild\/testReport/g' \
 | sed -E 's/buildStatus.icon[^\/]+\/([^\/]+)\/([a-z]+)/job\/Sling\/job\/modules\/job\/\1\/job\/\2\/badge\/icon/g'
